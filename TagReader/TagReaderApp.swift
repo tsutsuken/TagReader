9a11595049cc589c0e09f777917bf50dea4c7260
tsutsuken <592846+tsutsuken@users.noreply.github.com>
2023-03-03 22:17:37 +0900

    Initial Commit

---
+++ TagReader/TagReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TagReaderApp.swift
+//  TagReader
+//
+//  Created by Ken Tsutsumi on 2023/03/03.
+//
+
+import SwiftUI
+
+@main
+struct TagReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
